75fd8ca977824e5a7ea184d84a9813f84a4e45d5
nsdumont <nicole.s.dumont@gmail.com>
2020-10-19 23:10:03 -0400

    Updated shell files

--- run-files/goal_transfer_runs.sh
+++ run-files/goal_transfer_runs.sh
@@ -1,26 +1,27 @@
-python3 train.py --algo a2c --model MiniGrid_Goal_A2C --env MiniGrid-EmptyGoal-8x8-v0 --frames 60000 --seed 1
-python3 train.py --algo ppo --model MiniGrid_Goal_PPO --env MiniGrid-EmptyGoal-8x8-v0 --frames 60000 --seed 1
-python3 train.py --algo sr --model MiniGrid_Goal_SR --env MiniGrid-EmptyGoal-8x8-v0 --frames 60000 --seed 1 --curiosity True --lr 0.001 --use-V-advantage False --recon-loss-coef 2
+python3 train.py --algo a2c --model MiniGrid_Goal_A2C --env MiniGrid-EmptyGoal-8x8-v0 --frames 60000 --seed 1 --env-args "{switch_prob: 0}"
+python3 train.py --algo ppo --model MiniGrid_Goal_PPO --env MiniGrid-EmptyGoal-8x8-v0 --frames 60000 --seed 1 --env-args "{switch_prob: 0}"
 
-cp storage/MiniGrid_Goal_A2C/log.csv storage/MiniGrid_Goal_A2C/log1.csv
-cp storage/MiniGrid_Goal_PPO/log.csv storage/MiniGrid_Goal_PPO/log1.csv
-cp storage/MiniGrid_Goal_SR/log.csv storage/MiniGrid_Goal_SR/log1.csv
+python3 train.py --algo sr --model MiniGrid_Goal_SR_image --env MiniGrid-EmptyGoal-8x8-v0 --frames 60000 --input image --feature-learn curiosity --target-update 10 --recon-loss-coef 5 --entropy-coef 0.005 --batch-size 300 --frames-per-proc 100 --seed 1 --env-args "{switch_prob: 0}"
 
-python3 train.py --algo a2c --model MiniGrid_Goal_A2C --env MiniGrid-EmptyGoal-8x8-v0 --frames 120000 --seed 5 --lr 0.0005
-python3 train.py --algo ppo --model MiniGrid_Goal_PPO --env MiniGrid-EmptyGoal-8x8-v0 --frames 120000 --seed 5 --lr 0.0005
-python3 train.py --algo sr --model MiniGrid_Goal_SR --env MiniGrid-EmptyGoal-8x8-v0 --frames 120000 --seed 5 --curiosity True --lr 0.0005 --use-V-advantage False --entropy-coef 0.007 --recon-loss-coef 2
+python3 train.py --algo sr --model MiniGrid_Goal_SR_ssp --env MiniGrid-EmptyGoal-8x8-v0 --frames 60000 --input ssp --feature-learn curiosity --target-update 1 --recon-loss-coef 5 --entropy-coef 0.005 --batch-size 300 --frames-per-proc 10 --seed 1 --env-args "{switch_prob: 0}"
 
-cp storage/MiniGrid_Goal_A2C/log.csv storage/MiniGrid_Goal_A2C/log2.csv
-cp storage/MiniGrid_Goal_PPO/log.csv storage/MiniGrid_Goal_PPO/log2.csv
-cp storage/MiniGrid_Goal_SR/log.csv storage/MiniGrid_Goal_SR/log2.csv
 
-python3 train.py --algo a2c --model MiniGrid_Goal_A2C --env MiniGrid-EmptyGoal-8x8-v0 --frames 180000 --seed 4 --lr 0.0005
-python3 train.py --algo ppo --model MiniGrid_Goal_PPO --env MiniGrid-EmptyGoal-8x8-v0 --frames 180000 --seed 4 --lr 0.0005
-python3 train.py --algo sr --model MiniGrid_Goal_SR --env MiniGrid-EmptyGoal-8x8-v0 --frames 180000 --seed 4 --curiosity True --lr 0.0005 --use-V-advantage False --recon-loss-coef 2 --entropy-coef 0.008
 
-cp storage/MiniGrid_Goal_A2C/log.csv storage/MiniGrid_Goal_A2C/log3.csv
-cp storage/MiniGrid_Goal_PPO/log.csv storage/MiniGrid_Goal_PPO/log3.csv
-cp storage/MiniGrid_Goal_SR/log.csv storage/MiniGrid_Goal_SR/log3.csv
+python3 train.py --algo a2c --model MiniGrid_Goal_A2C --env MiniGrid-EmptyGoal-8x8-v0 --frames 120000 --seed 2 --env-args "{switch_prob: 0}"
+python3 train.py --algo ppo --model MiniGrid_Goal_PPO --env MiniGrid-EmptyGoal-8x8-v0 --frames 120000 --seed 2 --env-args "{switch_prob: 0}"
+python3 train.py --algo sr --model MiniGrid_Goal_SR_image --env MiniGrid-EmptyGoal-8x8-v0 --frames 120000 --input image --feature-learn curiosity --target-update 10 --recon-loss-coef 5 --entropy-coef 0.005 --batch-size 300 --frames-per-proc 100 --load-optimizer-state 0 --seed 2 --env-args "{switch_prob: 0}" 
+
+python3 train.py --algo sr --model MiniGrid_Goal_SR_ssp --env MiniGrid-EmptyGoal-8x8-v0 --frames 120000 --input ssp --feature-learn curiosity --target-update 1 --recon-loss-coef 5 --entropy-coef 0.005 --batch-size 300 --frames-per-proc 10 --load-optimizer-state 0 --seed 2 --env-args "{switch_prob: 0}"
+
+
+
+python3 train.py --algo a2c --model MiniGrid_Goal_A2C --env MiniGrid-EmptyGoal-8x8-v0 --frames 180000 --seed 3 --env-args "{switch_prob: 0}"
+python3 train.py --algo ppo --model MiniGrid_Goal_PPO --env MiniGrid-EmptyGoal-8x8-v0 --frames 180000 --seed 3 --env-args "{switch_prob: 0}"
+python3 train.py --algo sr --model MiniGrid_Goal_SR_image --env MiniGrid-EmptyGoal-8x8-v0 --frames 180000 --input image --feature-learn curiosity --target-update 10 --recon-loss-coef 5 --entropy-coef 0.005 --batch-size 300 --frames-per-proc 100 --load-optimizer-state 0 --seed 3 --env-args "{switch_prob: 0}"
+
+python3 train.py --algo sr --model MiniGrid_Goal_SR_ssp --env MiniGrid-EmptyGoal-8x8-v0 --frames 180000 --input ssp --feature-learn curiosity --target-update 1 --recon-loss-coef 5 --entropy-coef 0.005 --batch-size 300 --frames-per-proc 10 --load-optimizer-state 0 --seed 3 --env-args "{switch_prob: 0}"
+
+
 
 
 
--- run-files/size_transfer_runs.sh
+++ run-files/size_transfer_runs.sh
@@ -1,23 +1,23 @@
 python3 train.py --algo a2c --model MiniGrid_size_A2C --env MiniGrid-Empty-6x6-v0 --frames 50000 --seed 1
 python3 train.py --algo ppo --model MiniGrid_size_PPO --env MiniGrid-Empty-6x6-v0 --frames 50000 --seed 1
-python3 train.py --algo sr --model MiniGrid_size_SR --env MiniGrid-Empty-6x6-v0 --frames 50000 --seed 1 --curiosity True --lr 0.001 --use-V-advantage False --recon-loss-coef 2 
+python3 train.py --algo sr --model MiniGrid_size_SR_image --env MiniGrid-Empty-6x6-v0 --frames 50000 --input image --feature-learn curiosity --target-update 10 --recon-loss-coef 5 --entropy-coef 0.005 --batch-size 300 --frames-per-proc 100 --seed 1
+python3 train.py --algo sr --model MiniGrid_size_SR_ssp --env MiniGrid-Empty-6x6-v0 --frames 50000 --input ssp --feature-learn curiosity --target-update 1 --recon-loss-coef 5 --entropy-coef 0.005 --batch-size 300 --frames-per-proc 10 --seed 1 
 
-cp storage/MiniGrid_size_A2C/log.csv storage/MiniGrid_size_A2C/log1.csv
-cp storage/MiniGrid_size_PPO/log.csv storage/MiniGrid_size_PPO/log1.csv
-cp storage/MiniGrid_size_SR/log.csv storage/MiniGrid_size_SR/log1.csv
+
 
 python3 train.py --algo a2c --model MiniGrid_size_A2C --env MiniGrid-Empty-8x8-v0 --frames 100000 --seed 1
 python3 train.py --algo ppo --model MiniGrid_size_PPO --env MiniGrid-Empty-8x8-v0 --frames 100000 --seed 1
-python3 train.py --algo sr --model MiniGrid_size_SR --env MiniGrid-Empty-8x8-v0 --frames 100000 --seed 1 --curiosity True --lr 0.001 --use-V-advantage False --recon-loss-coef 2 
+python3 train.py --algo sr --model MiniGrid_size_SR_image --env MiniGrid-Empty-8x8-v0 --frames 100000 --input image --feature-learn curiosity --target-update 10 --recon-loss-coef 5 --entropy-coef 0.005 --batch-size 300 --frames-per-proc 100 --load-optimizer-state 0 --seed 1
+python3 train.py --algo sr --model MiniGrid_size_SR_ssp --env MiniGrid-Empty-8x8-v0 --frames 100000 --input ssp --feature-learn curiosity --target-update 1 --recon-loss-coef 5 --entropy-coef 0.005 --batch-size 300 --frames-per-proc 10 --load-optimizer-state 0 --seed 1 
 
-cp storage/MiniGrid_size_A2C/log.csv storage/MiniGrid_size_A2C/log2.csv
-cp storage/MiniGrid_size_PPO/log.csv storage/MiniGrid_size_PPO/log2.csv
-cp storage/MiniGrid_size_SR/log.csv storage/MiniGrid_size_SR/log2.csv
+
+
 
 python3 train.py --algo a2c --model MiniGrid_size_A2C --env MiniGrid-Empty-16x16-v0 --frames 150000 --seed 1
 python3 train.py --algo ppo --model MiniGrid_size_PPO --env MiniGrid-Empty-16x16-v0 --frames 150000 --seed 1
-python3 train.py --algo sr --model MiniGrid_size_SR --env MiniGrid-Empty-16x16-v0 --frames 150000 --seed 1 --curiosity True --lr 0.001 --use-V-advantage False --entropy-coef 0.007 --recon-loss-coef 2
+python3 train.py --algo sr --model MiniGrid_size_SR_image --env MiniGrid-Empty-16x16-v0 --frames 150000 --input image --feature-learn curiosity --target-update 10 --recon-loss-coef 5 --entropy-coef 0.005 --batch-size 300 --frames-per-proc 100 --load-optimizer-state 0 --seed 1
+python3 train.py --algo sr --model MiniGrid_size_SR_ssp --env MiniGrid-Empty-16x16-v0 --frames 150000 --input ssp --feature-learn curiosity --target-update 1 --recon-loss-coef 5 --entropy-coef 0.005 --batch-size 300 --frames-per-proc 10 --load-optimizer-state 0 --seed 1 
 
-cp storage/MiniGrid_size_A2C/log.csv storage/MiniGrid_size_A2C/log3.csv
-cp storage/MiniGrid_size_PPO/log.csv storage/MiniGrid_size_PPO/log3.csv
-cp storage/MiniGrid_size_SR/log.csv storage/MiniGrid_size_SR/log3.csv
+
+
+
